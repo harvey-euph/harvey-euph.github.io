e9513853039e10468c483889520c69c205832918
Harvey <harvey@aargo.trade>
2025-08-15 23:23:56 +0800

    auto toggle

--- _plugins/toggle_block.rb
+++ _plugins/toggle_block.rb
@@ -7,15 +7,23 @@ module Jekyll
 
     def render(context)
       # 自動啟用 toggle CSS/JS
-      # page = context.registers[:page]
-      # page['use_toggle'] = true
+      page = context.registers[:page]
+      page['use_toggle'] = true
 
+      site = context.registers[:site]
+      converter = site.find_converter_instance(Jekyll::Converters::Markdown)
+
+      # 先取得 block 內容
       content = super.strip
+
+      # 渲染為 HTML（支援 Markdown 語法）
+      content_html = converter.convert(content)
+
       <<~HTML
       <div class="toggle">
         <div class="toggle-header">#{@title}</div>
         <div class="toggle-content">
-          #{Kramdown::Document.new(content).to_html}
+          #{content_html}
         </div>
       </div>
       HTML
--- _tabs/about.md
+++ _tabs/about.md
@@ -5,9 +5,6 @@ order: 4
 title: About me
 ---
 
-<link rel="stylesheet" href="{{ '/assets/css/toggle.css' | relative_url }}">
-<script src="{{ '/assets/js/toggle.js' | relative_url }}"></script>
-
 👋 Hi, I’m Harvey — I graduated from NTU with a degree in Mathematics in 2023.
 This little corner of the internet is where I share pieces of my life, my career journey, and the projects that have shaped me — both the ones I’m working on now and the ones I’ve completed.
 Take your time, look around, and discover whichever part of my story sparks your curiosity.
