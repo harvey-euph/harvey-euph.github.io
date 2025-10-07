2efc46edd3d80ade883ead4f8f13eeaed267efd3
Harvey Wu <harvey@HARVEY-EXPERT.localdomain>
2025-10-07 19:51:47 +0800

    update content.

--- _scripts/fetch_s3_images.py
+++ _scripts/fetch_s3_images.py
@@ -1,7 +1,7 @@
 import os
 import boto3
 
-bucket = os.environ['S3_BUCKET']
+bucket_name = os.environ['S3_BUCKET']
 region_name = os.environ.get('AWS_REGION', 'ap-northeast-1')
 local_dir = os.environ.get('LOCAL_DIR', 'asset/img-from-aws-s3')
 
@@ -11,10 +11,15 @@ s3 = boto3.client('s3', region_name=region_name)
 
 # 列出 bucket 下所有物件
 paginator = s3.get_paginator('list_objects_v2')
-for page in paginator.paginate(Bucket=bucket):
+for page in paginator.paginate(Bucket=bucket_name):
     for obj in page.get('Contents', []):
-        key = obj["Key"]
+        key = obj['Key']
+        if not key.lower().endswith(('.png', '.jpg', '.jpeg', '.gif', '.webp')):
+            continue  # 只抓圖片
+
+        # 本地檔案路徑
         local_path = os.path.join(local_dir, key)
-        os.makedirs(os.path.dirname(local_path), exist_ok=True)
-        s3.download_file(bucket, key, local_path)
+        s3.download_file(bucket_name, key, local_path)
         print(f"Downloaded {key} -> {local_path}")
+
+
